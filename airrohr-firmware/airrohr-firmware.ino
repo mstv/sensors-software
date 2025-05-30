14e7cc0f66f33206c50278b92b76ea071833bb62
mstv <mstv@gmx.net>
2025-05-30 11:21:20 +0200

    fix(loop): Update display after fetching sensors

--- airrohr-firmware/airrohr-firmware.ino
+++ airrohr-firmware/airrohr-firmware.ino
@@ -6113,13 +6113,6 @@ void loop(void)
 		}
 	}
 
-	if ((msSince(last_display_millis) > DISPLAY_UPDATE_INTERVAL_MS) &&
-		(cfg::has_display || cfg::has_sh1106 || lcd_1602 || lcd_2004))
-	{
-		display_xvalues();
-		last_display_millis = act_milli;
-	}
-
 	server.handleClient();
 	yield();
 
@@ -6305,6 +6298,13 @@ void loop(void)
 		sum_send_time = 0;
 		starttime = millis(); // store the start time
 		count_sends++;
+	}
+
+	if ((msSince(last_display_millis) > DISPLAY_UPDATE_INTERVAL_MS) &&
+		(cfg::has_display || cfg::has_sh1106 || lcd_1602 || lcd_2004))
+	{
+		display_xvalues();
+		last_display_millis = act_milli;
 	}
 
 #if defined(ESP8266)
